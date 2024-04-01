c1c2792c215727d293c7fab85931b1b791d3879b
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-04-01 20:03:18 +0900

    ユーザー情報編集機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_with_password(user_params)
+      bypass_sign_in(@user) 
+      redirect_to root_path, notice: 'Profile was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :current_password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'results/index'
   root to: "results#index"
+  resources :users, only: [:edit, :update]
 end
