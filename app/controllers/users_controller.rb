edcd35d573b8d4570bc569a56f41eba527357e31
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-04-10 18:33:07 +0900

    マイページ実装②
    ログインページに推移できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = current_user
-    redirect_to root_path, alert: 'ログインしてください' unless @user
+    @results = @user.results 
   end
 
   def update
@@ -11,17 +11,20 @@ class UsersController < ApplicationController
     if @user.update_with_password(user_params)
       bypass_sign_in(@user)
       redirect_to root_path, notice: 'Profile was successfully updated.'
-    else
-      render :edit, status: :unprocessable_entity
     end
   end
 
   def show
-  @user = User.find(params[:id])
-  @results = @user.results 
-  @nickname = @user.nickname 
- end
-
+    if params[:id] != 'edit' && params[:id] != 'sign_in'
+      if user_signed_in?
+        @user = User.find(params[:id])
+        @results = @user.results 
+        @nickname = @user.nickname 
+      else
+        redirect_to new_user_session_path, alert: 'ログインしてください'
+      end
+    end
+  end
 
   private
 
