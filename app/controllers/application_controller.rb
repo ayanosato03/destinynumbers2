ef4bc49fc23f2d1f74a8a46ee1431faa8eae8e3f
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-04-11 18:41:41 +0900

    ヘッダー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+
+  def user_signed_in?
+    current_user.present?
+  end
+  
 end
