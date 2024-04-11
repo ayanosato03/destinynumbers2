e3e7dc81b46ee5c13a65e32bcc67447513dc54db
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-04-11 19:34:09 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   def after_sign_in_path_for(_resource)
     root_path 
@@ -19,5 +20,10 @@ class ApplicationController < ActionController::Base
   def user_signed_in?
     current_user.present?
   end
-  
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
 end
