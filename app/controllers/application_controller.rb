fbc2d158ad3efd56c7004dceb91fc2aea864988c
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-04-11 19:34:49 +0900

    rubocop実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   def after_sign_in_path_for(_resource)
-    root_path 
+    root_path
   end
 
   def after_sign_out_path_for(_resource_or_scope)
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,32 @@
 class UsersController < ApplicationController
-before_action :authenticate_user!, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
 
-
-  def new 
+  def new
     @user = User.new
   end
 
-
   def edit
     @user = current_user
-    @results = @user.results 
+    @results = @user.results
   end
 
   def update
     @user = current_user
-    if @user.update_with_password(user_params)
-      bypass_sign_in(@user)
-      redirect_to root_path, notice: 'Profile was successfully updated.'
-    end
+    return unless @user.update_with_password(user_params)
+
+    bypass_sign_in(@user)
+    redirect_to root_path, notice: 'Profile was successfully updated.'
   end
 
   def show
-    if params[:id] != 'edit' && params[:id] != 'sign_in'
-      if user_signed_in?
-        @user = User.find(params[:id])
-        @results = @user.results 
-        @nickname = @user.nickname 
-      else
-        redirect_to new_user_session_path, alert: 'ログインしてください'
-      end
+    return unless params[:id] != 'edit' && params[:id] != 'sign_in'
+
+    if user_signed_in?
+      @user = User.find(params[:id])
+      @results = @user.results
+      @nickname = @user.nickname
+    else
+      redirect_to new_user_session_path, alert: 'ログインしてください'
     end
   end
 
