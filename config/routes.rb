0c6f02c7cac61f912541c0805346b057b8a163cd
ayanosato03 <159245312+ayanosato03@users.noreply.github.com>
2024-04-20 15:57:25 +0900

    numbersリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :results, only: [:index, :create, :show]
   get '/calculation_results', to: redirect('/')  # リダイレクトルーティング
   get '/result.js', to: 'results#javascript_response'
-  resources :life_pass_numbers, only: [:index, :show]
+  resources :life_pass_numbers, only: [:index] do
+   collection do
+    get :numbers1
+    end
+  end
 end
